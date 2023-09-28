58764b9a48855ce086ae934d6f547ac3de41f003
KlincovaAnastasiya <aklin-93@yandex.ru>
2023-09-28 12:01:41 +0300

    создана БД, без комментариев

--- tipatrello/projects/admin.py
+++ tipatrello/projects/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from django_mptt_admin.admin import DjangoMpttAdmin
-from .models import Worker
+from .models import Worker, Task, Project
 
 @admin.register(Worker)
 class WorkerAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):    
+    list_display = (
+        'name', 'deadline', 'chief', 'is_done'
+    )    
+
 
 
 
--- tipatrello/projects/models.py
+++ tipatrello/projects/models.py
@@ -29,5 +29,69 @@ class Worker(models.Model):
         return self.name  
 
 
+class Task(models.Model):
+    "Связь сотрудника с конкретной задачей."
     
+    name = models.CharField(
+        'Название задачи',
+        max_length=50,
+    )
+    is_done = models.BooleanField()
+    
+    description = models.TextField(
+        'Описание задачи',
+        max_length=500,
+    )
+
+    workers = models.ManyToManyField(
+        Worker,               
+        related_name='task_and_workers',
+        verbose_name='исполнитель задачи',
+    )    
+    
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):    
+
+    name = models.CharField(
+        'Название проекта',
+        max_length=20,
+    )
+    deadline = models.DateField(
+        'Срок исполнения',
+    )
+    description = models.TextField(
+        'Описание проекта',
+        max_length=500,
+    )
+    chief = models.ForeignKey(        
+        Worker,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='project_as_chief',
+        verbose_name='Руководитель проекта',
+    )    
+    tasks = models.ManyToManyField(
+        Task,        
+        #on_delete=models.DO_NOTHING, #если удаляют задачу, то ничего не происходит
+        #related_name='project',
+        verbose_name='Задачи проекта',       
+        related_name='projects',
+    )
+
+    is_done = models.BooleanField()
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+    def __str__(self):
+        return self.name
 
